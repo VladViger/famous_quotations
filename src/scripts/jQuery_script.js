7982ccad5078e278c540e03009879b9f4dadacde
VladViger <vlad.v-mail@yandex.ru>
2016-03-25 21:58:09 +0200

    complete header & toolbar

--- src/scripts/jQuery_script.js
+++ src/scripts/jQuery_script.js
@@ -1,3 +1,9 @@
 $('.btn-about').on('click', function() {
 	$('.description').toggleClass('hide-xs');
+	$('.btn-about .icon').toggleClass('flaticon-cross');
+});
+
+$('.sort').on('click', function() {
+	$(this).toggleClass('active');
+	$('.toolbar [class*="sort-"]').toggleClass('mobile-height');
 });
