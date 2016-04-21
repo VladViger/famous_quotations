147edbabec73773e4535ef0ba69759945d541d7e
VladViger <vlad.v-mail@yandex.ru>
2016-04-21 22:08:45 +0300

    v.0.0.9

--- src/scripts/bb_models.js
+++ src/scripts/bb_models.js
@@ -7,21 +7,28 @@ var Article = Backbone.Model.extend({
 	},
 
 	initialize: function() {
+		this.calcRating();
+
 		if (!this.get('theDate')) {
-			this.set({'theDate': this.getDateString()});
+			var date = new Date();
+			this.set({'theDate': date});
+			this.set({'theDateString': this.getDateString(date)});
 		}
 	},
 
-	getDateString: function() {
-		var date, day, month, year;
-		date = new Date();
+	getDateString: function(date) {
+		var day, month, year;
 		day = ('0' + date.getDate()).slice(-2);
 		month = ('0' + (date.getMonth() + 1)).slice(-2);
 		year = date.getFullYear();
 		return (day + '-' + month + '-' + year);
 	},
 
-	getRating: function() {
-		return this.get('liked').length;
+	calcRating: function() {
+		this.set('rating', this.get('liked').length);
+	},
+
+	clear: function() {
+		this.destroy();
 	}
 });
--- src/scripts/bb_views.js
+++ src/scripts/bb_views.js
@@ -4,27 +4,37 @@ var ArticleView = Backbone.View.extend({
 	template: _.template($('#article-template').html()),
 
 	events: {
-
+		'click .delete.active': 'clear'
 	},
 
 	initialize: function() {
-		
+		this.model.bind('destroy', this.remove, this);
 	},
 
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
+		this.setRights(this.$el);
 		return this;
+	},
+
+	setRights: function(el) {
+		console.log(USER_IP);
+		if (this.model.get('liked').indexOf(USER_IP) == -1) return;
+		el.find('.delete').addClass('active');
+	},
+
+	clear: function() {
+		this.model.clear();
 	}
 });
 
 
-// temp
 var AppView = Backbone.View.extend({
 	el: $('#toolbar'),
 
 	initialize: function() {
-		articleList.bind('add', this.addOne, this);
 		this.addAll(articleList);
+		articleList.bind('add', this.addOne, this);
 	},
 
 	render: function() {
